01a1c036768d5a77f0430ad01e1e24ee680ed286
tarugatto <talgat.zhaimenov@gmail.com>
2022-02-27 19:20:44 +0600

    Solution for second homework Golang Basics. Calculate area of shapes

--- solution.go
+++ solution.go
@@ -1,4 +1,4 @@
-package square
+package main
 
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
@@ -8,5 +8,40 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	SidesTriangle = 3
+	SidesSquare   = 4
+	SidesCircle   = 0
+)
+
+func main() {
+	fmt.Println("Area of shapes")
+	CalcSquare(8, SidesTriangle)
+	CalcSquare(10, SidesSquare)
+	CalcSquare(12, SidesCircle)
+}
+
+func CalcSquare(sideLen float64, sidesNum uint64) float64 {
+
+	var result float64
+	switch sidesNum {
+	case SidesTriangle:
+		result = math.Sqrt(3.0) / 4.0 * math.Pow(sideLen, 2)
+		fmt.Println("The area of a triangle is equal to:", result)
+	case SidesSquare:
+		result = sideLen * sideLen
+		fmt.Println("The area of a square is equal to:", result)
+	case SidesCircle:
+		result = math.Pi * sideLen * sideLen
+		fmt.Println("The area of a circle is equal to:", result)
+	default:
+		fmt.Println("This value is not supported")
+	}
+
+	return result
 }
